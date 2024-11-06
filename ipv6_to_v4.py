b82214bc0ab82fb91c01acc9335550afb455db48
Speedy <148458995+speedyownjoo@users.noreply.github.com>
2024-11-05 18:42:19 -0600

    works

---
+++ ipv6_to_v4.py
@@ -0,0 +1,18 @@
+from ipaddress import IPv6Address, IPv4Address
+from sys import stdin, stdout, stderr
+
+
+def main(ip_str) -> str:
+    try:
+        ip: IPv6Address = IPv6Address(ip_str)
+        ipv4: IPv4Address = ip.ipv4_mapped
+        return str(ipv4)
+    except Exception as e:
+        print(f'ERROR converting {ip_str}: {e}', file=stderr)
+        return ip_str
+
+
+if __name__ == '__main__':
+    for line in stdin:
+        stdout.write(main(ip_str=line.rstrip()))
+        stdout.flush()
